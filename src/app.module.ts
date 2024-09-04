5f86cf1badff99bb04081892ecf406d0b8d78b17
Mircea Casapu <mircea.casapu@gmail.com>
2024-09-04 23:18:00 +0300

    feat: bootstrap graphql and invoice resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { InvoiceModule } from './invoice/invoice.module';
+import { join } from 'path';
 
 @Module({
-  imports: [DbModule],
+  imports: [
+    DbModule,
+    InvoiceModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
